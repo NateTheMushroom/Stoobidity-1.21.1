1f30d2584608029d473904f4c1bca71417687485
NateTheMushroom <nate09tgd@gmail.com>
2026-01-01 22:11:53 -0500

    tooltips!

--- src/main/java/net/mshm/stoobidity/item/ModItems.java
+++ src/main/java/net/mshm/stoobidity/item/ModItems.java
@@ -3,22 +3,39 @@ package net.mshm.stoobidity.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.mshm.stoobidity.Stoobidity;
 import net.mshm.stoobidity.item.custom.ShizelItem;
+
+import java.util.List;
 
 public class ModItems {
 //~~ITEMS~~
     public static final Item PETRIFIED_POO = registerItem("petrified_poo", new Item(new Item.Settings()));
     public static final Item POO = registerItem("poo", new Item(new Item.Settings()));
     public static final Item VEIN_SHARD = registerItem("vein_shard", new Item(new Item.Settings()));
-    public static final Item NOT_A_CROSS = registerItem("not_a_cross", new Item(new Item.Settings()));
+    public static final Item NOT_A_CROSS = registerItem("not_a_cross", new Item(new Item.Settings()) {
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            tooltip.add(Text.translatable("tooltip.stoobidity.not_a_cross.tooltip"));
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    });
     //~~Custom Items~~
     public static final Item SHIZEL = registerItem("shizel", new ShizelItem(new Item.Settings().maxDamage(600)));
     //~~Food Items~~
-    public static final Item HALLS = registerItem("halls", new Item(new Item.Settings().food(ModFoodComponents.HALLS)));
+    public static final Item HALLS = registerItem("halls", new Item(new Item.Settings().food(ModFoodComponents.HALLS)) {
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            tooltip.add(Text.translatable("tooltip.stoobidity.halls.tooltip"));
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    });
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Stoobidity.MOD_ID, name), item);
--- src/main/java/net/mshm/stoobidity/item/custom/ShizelItem.java
+++ src/main/java/net/mshm/stoobidity/item/custom/ShizelItem.java
@@ -2,16 +2,21 @@ package net.mshm.stoobidity.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.Map;
 
 public class ShizelItem extends Item {
@@ -51,5 +56,15 @@ public class ShizelItem extends Item {
         }
 
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.stoobidity.shizel.shift_down"));
+        } else {
+            tooltip.add(Text.translatable("tooltip.stoobidity.shizel"));
+        }
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
